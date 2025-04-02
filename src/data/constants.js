29bcb392f3c366005d32e7d4d8b3c9772294f1b2
Sunny Kumar <sk202542@gmail.com>
2025-04-02 18:00:29 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/SunnyKumar-code",
   resume:
-    "https://drive.google.com/file/d/12xWIHscdB7tZDSpOCJRriAaEWqtn3rW0/view?usp=sharing",
+    "https://drive.google.com/file/d/1nY8XoE1mhhSJOKkym2QUfso0s2B1291H/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/sunny-kumar-coder/",
   insta: "https://www.instagram.com",
   facebook: "https://www.facebook.com",
